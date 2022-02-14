27b5e510724cbe0c2b48c71d8b0deea48798175d
Evgeny Matusevich <ma2sevich222@gmail.com>
2022-02-14 21:57:17 +1000

    отформатировал внешку

--- losses.py
+++ losses.py
@@ -2,17 +2,20 @@ import tensorflow as tf
 from keras import backend as K
 
 
+#  Евклидово расстояние
 def euclid_dis(vects):
     x, y = vects
     sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
     return K.sqrt(K.maximum(sum_square, K.epsilon()))
 
 
+# Выходной размер
 def eucl_dist_output_shape(shapes):
     shape1, shape2 = shapes
     return (shape1[0], 1)
 
 
+# Функция ошибки contrastive_loss
 def contrastive_loss(y_true, y_pred):
     y_true = tf.dtypes.cast(y_true, tf.float64)
     y_pred = tf.dtypes.cast(y_pred, tf.float64)
