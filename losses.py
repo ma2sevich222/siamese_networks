2e9a8eea827a7019841f5ff225ee4e448975906f
Evgeny Matusevich <ma2sevich222@gmail.com>
2022-02-14 21:30:01 +1000

    добавил функции ошибки losses.py

---
+++ losses.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from keras import backend as K
+
+
+def euclid_dis(vects):
+    x, y = vects
+    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+    return K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
+
+
+def contrastive_loss(y_true, y_pred):
+    y_true = tf.dtypes.cast(y_true, tf.float64)
+    y_pred = tf.dtypes.cast(y_pred, tf.float64)
+    margin = 1
+    square_pred = K.square(y_pred)
+    margin_square = K.square(K.maximum(margin - y_pred, 0))
+    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
